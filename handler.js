8711ab990089694ef9375e34bcd0bed5f354b67d
Neil Farrington <me@neilfarrington.com>
2017-08-28 21:32:15 +0000

    Initial commit

---
+++ handler.js
@@ -0,0 +1,94 @@
+const cacheFile = '/tmp/vatsim-data-cache';
+const cacheLife = 60 * 60 * 3; // 3 hours
+const statusUrl = 'https://status.vatsim.net/';
+
+const dataServerBlacklist = [
+    'http://data.vattastic.com/vatsim-data.txt', // presents the wrong encoding type causing replacement characters
+];
+
+const cache = require('node-file-cache').create({ file: cacheFile, life: cacheLife });
+const request = require('request');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+let dataTimestamp = null;
+
+function getDataServer() {
+    return new Promise(function(resolve, reject) {
+        getDataServers().then(function (servers) {
+            resolve(servers[Math.floor(Math.random() * servers.length)]);
+        });
+    });
+}
+
+function getDataServers() {
+    return new Promise(function(resolve, reject) {
+        let cachedServers = cache.get('vatsim-data-servers');
+
+        if (cachedServers) {
+            resolve(cachedServers);
+        } else {
+            getFreshDataServers().then(function (servers) {
+                resolve(servers);
+            });
+        }
+    });
+}
+
+function getFreshDataServers() {
+    return new Promise(function(resolve, reject) {
+        let servers = [];
+        request(statusUrl, function(error, response, body) {
+            if (!error) {
+                const lines = body.replace(/\r/g, '').split('\n');
+                for (let i = 0; i < lines.length; i++) {
+                    if (lines[i].substring(0, 5) === 'url0=') {
+                        let server = lines[i].substring(5);
+                        if (dataServerBlacklist.indexOf(server) === -1) {
+                            servers.push(lines[i].substring(5));
+                        }
+                    }
+                }
+
+                cache.set('vatsim-data-servers', servers);
+
+                resolve(servers);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
+
+function getData(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function(error, response, body) {
+            if (!error) {
+                // strip comments from the file
+                let timestampMatch = body.match(/!GENERAL:[^!]*UPDATE = ([\d]*)/);
+                if (timestampMatch) {
+                    dataTimestamp = timestampMatch[1];
+                    resolve(body.replace(/^;[^\n]*\n/gm, ''));
+                } else {
+                    reject(Error('Data timestamp not valid: ' + dataTimestamp))
+                }
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
+
+module.exports.store = (event, context, callback) => {
+    getDataServer()
+        .then(getData)
+        .then(function uploadToS3(data) {
+            return s3.upload({
+                Bucket: process.env.BUCKET,
+                Key: dataTimestamp + '.txt',
+                Body: data,
+            }).promise();
+        })
+        .then(success => callback(null, success))
+        .catch(callback);
+};
